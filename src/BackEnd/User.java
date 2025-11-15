37ce7224914e5faee698e71307fcce547ea76d98
lixvin1 <hussein.imadian@gmail.com>
2025-11-15 02:00:35 +0200

    adjusted classes and finished basic implenation on them

--- src/BackEnd/User.java
+++ src/BackEnd/User.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package BackEnd;
 
-/**
- *
- * @author husse
- */
-public class User {
+
+
+
+
+
+public abstract class  User implements Records {
+    
+    private String userId;
+    private String username;
+    private String email;
+    private String passwordHash;
+    private String role;
+    
+     public User(String userId, String username, String email, String passwordHash, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+  
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    @Override
+ public String getSearchKey() {
+        return userId;
+    }
     
 }
