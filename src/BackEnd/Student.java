d9f633870ffba65e5563a90c1c5fd1e93db90dd9
Ayman71 <ayman.elsharif1@gmail.com>
2025-11-23 07:38:38 +0200

    Fixed merge bug

--- src/BackEnd/Student.java
+++ src/BackEnd/Student.java
@@ -124,21 +124,4 @@ public class Student extends User {
         }
         return null;
     }
-    return null;
-}
-
-    public Map<String, QuizAttempt> getQuizAttempts() {
-        return quizAttempts;
-    }
-
-    public void setQuizAttempts(Map<String, QuizAttempt> quizAttempts) {
-        this.quizAttempts = quizAttempts;
-    }
-
-    public void setCertificates(ArrayList<Certificate> certificates) {
-        this.certificates = certificates;
-    }
-
-
-
 }
